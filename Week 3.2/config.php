993df97a95227723ea462406024fddce8fce672b
bluefox62 <brett.riegert@smail.rasmussen.edu>
2017-05-28 16:00:25 -0500

    project2

---
+++ Week 3.2/config.php
@@ -0,0 +1,7 @@
+<?php
+// This is an example of config.php
+$servername = 'us-cdbr-azure-southcentral-f.cloudapp.net';
+$username = 'bbb5db32b1a274';
+$password = 'e782bbe2';
+$dbname = 'project2';
+?>
