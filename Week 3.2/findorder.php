8491c086e5f70a6d7c2d2100fba93a4d72c03bbb
bluefox62 <brett.riegert@smail.rasmussen.edu>
2017-05-28 16:33:22 -0500

    changes

--- Week 3.2/findorder.php
+++ Week 3.2/findorder.php
@@ -23,23 +23,23 @@
 					include 'config.php';
 					include 'opendb.php';
 
-					$conf_num = (isset($_POST['conf_num'])    ? $_POST['conf_num']   : '');
+					$fname = (isset($_POST['fname'])    ? $_POST['fname']   : '');
 
-					$sql= "SELECT customers.customerid, customers.fname, customers.lname, customers.state, orders.conf_num, orders.salesrep
-						FROM customers
-						JOIN orders on customers.customerid = orders.customerid
-						WHERE conf_num LIKE '$conf_num' LIMIT 100";
+					$sql= "SELECT contact.id, contact.email, contact.phone, location.city, location.sadd,
+						FROM contact
+						JOIN location on contact.fname = location.fname
+						WHERE fname LIKE '$fname' LIMIT 100";
 					$result = mysqli_query($conn, $sql);
 
 					if (mysqli_num_rows($result) > 0) {
 					    // output data of each row
 					    while($row = mysqli_fetch_assoc($result)) {
-									echo "ID: " . $row["customerid"]. "<br>";
-					        echo "First Name: " . $row["fname"]. "<br>";
-					        echo "Last Name: " . $row["lname"]. "<br>";
-									echo "State: " . $row["state"]. "<br>";
-									echo "Conf. #: " . $row["conf_num"]. "<br>";
-									echo "Sales Rep: " . $row["salesrep"]. "<br>";
+									echo "ID: " . $row["id"]. "<br>";
+					        echo "Email: " . $row["email"]. "<br>";
+					        echo "Phone #: " . $row["phone"]. "<br>";
+									echo "City: " . $row["city"]. "<br>";
+									echo "Address: " . $row["location"]. "<br>";
+									echo "Name: " . $row["fname"]. "<br>";
 					    }
 					} else {
 					    echo "0 results";
